70804c65cc14a4617b6c1a8c9356cf20755cc433
2ninjask2 <hsjdog@jejunu.ac.kr>
2023-10-18 18:02:08 +0900

    Initial commit

---
+++ config/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import redirect #render
+
+def index(request):
+    if request.user.is_authenticated:
+        return redirect("/posts/feeds/")
+    else:
+        return redirect("/users/login/")
+    #return render(request, "index.html")
